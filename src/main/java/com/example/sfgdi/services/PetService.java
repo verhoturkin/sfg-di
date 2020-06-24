7defcda3dc9082efd80a636f169e28d60fb42679
Verkhoturkin <verhoturkin@gmail.com>
2020-06-24 17:40:08 +0500

    assignment

---
+++ src/main/java/com/example/sfgdi/services/PetService.java
@@ -0,0 +1,12 @@
+package com.example.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+@Service
+public interface PetService {
+
+    String getPetType();
+}
